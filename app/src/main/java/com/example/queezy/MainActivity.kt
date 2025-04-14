e1211fd29433c2e235e6620e6e19ae39af08599d
Odiljon <afewminutes777@gmail.com>
2025-04-14 23:56:53 +0500

    Started

---
+++ app/src/main/java/com/example/queezy/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.queezy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.queezy.ui.theme.QueezyTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QueezyTheme {
+
+                val sheetState = rememberModalBottomSheetState()
+                var isSheetOpen by rememberSaveable { mutableStateOf(false) }
+
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        Button(
+                            onClick = {
+                                isSheetOpen = true
+                            }
+                        ) {
+                            Text("hello")
+                        }
+                    }
+                    if (isSheetOpen) {
+                        ModalBottomSheet(
+                            modifier = Modifier.fillMaxWidth().height(800.dp),
+                            sheetState = sheetState,
+                            onDismissRequest = {
+                                isSheetOpen = false
+                            }
+                        ) {
+                            Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+/*
+
+50 -ta flag
+youtube type chooser
+if chosen was right some type of congrats
+when user closes application it wont store but it always will be mashed up
+ */
